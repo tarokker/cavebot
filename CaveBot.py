4541b28aea4730b5a84acfec1725dbaf6c5bd1db
root <root@debian11-vm-temp.homenet.telecomitalia.it>
2022-07-27 19:35:04 +0200

    rimosso /cave

--- CaveBot.py
+++ CaveBot.py
@@ -16,14 +16,11 @@ global games
 
 # Display an help text
 def help_text(update):
-	return "@cave_bot - Play Colossal Cave Adventure!\n" \
+	return "@maioadventuretest_bot - Play Colossal Cave Adventure!\n" \
 		"This is the list of commands:\n\n" \
 		"/restart - Starts a new game\n" \
-		"/cave - Send a command (Eg. /cave look)\n" \
-		"/help - Display this help text.\n\n" \
-		"Created by: The Electric Ant\n" \
-		"If you like this game please consider making a donation " \
-		"https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=XSKMSYEX77FZ4"
+		"/help - Display this help text.\n" \
+		"command - send a command ingame (Eg. look)\n\n"
 
 # Cleanup and save game before quitting
 def exitGracefully(signal, frame):
@@ -51,27 +48,39 @@ def start(update):
 # process user input
 def process_input(update):
 	global games
-	
+
 	game = games.getGame(update.chat.id)
 	if game == None:
 		return "Sorry, the server is busy. Try again later."
-	
-	if len(update.text) >= 7:
-		return game.execCmd(update.text[6:])
-	else:
-		return game.getOutput()
+
+	return game.execCmd(update.text)
+	#if len(update.text) >= 7:
+	#	return game.execCmd(update.text[6:])
+	#else:
+	#	return game.getOutput()
 
 # Start a new game
 def restart(update):
 	game = games.getGame(update.chat.id)
-	
 	game.restart()
 	return game.getOutput()
+
+def catchall(update):
+	if update.text.lower() == "/start":
+		return help_text(update) # start(update)
+	elif update.text.lower() == "/help":
+		return help_text(update)
+	elif update.text.lower() == "/restart":
+		return restart(update)
+	return process_input(update)
+
+def catchalltrigger(update):
+	return True
 
 # Log received messages
 def logger(update):
 	print("%i > %s" % (update.chat.id, update.text))
-	
+
 #
 # MAIN:
 #
@@ -86,9 +95,11 @@ if __name__ == "__main__":
 	bot = botbuilder.BotBuilder(apikey_file="apikey.txt")
 	# bot actions
 	bot.do_when(lambda update: (True), logger, botbuilder.DO_NOT_CONSUME)
-	bot.send_message_when("start", start)
-	bot.send_message_when("cave", process_input)
-	bot.send_message_when("help", help_text)
-	bot.send_message_when("restart", restart)
+	#bot.send_message_when("start", start)
+	#bot.send_message_when("cave", process_input)
+	#bot.send_message_when("help", help_text)
+	#bot.send_message_when("restart", restart)
+	bot.send_message_when(catchalltrigger, catchall)
+
 	# create bot and launch it
 	bot.build().start()
